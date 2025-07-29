3a90e42ba1528c7cabed0f5ed1d310e4d03d6607
Tanzeela Memon <tanzeelamemon.bscsf22@iba-suk.edu.pk>
2025-07-29 22:33:56 +0500

    Home Page Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import GalleryPage from "./pages/GalleryPage";
+
 
 function App() {
   return (
-    <div className=" min-h-screen">
-   
-      <GalleryPage />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+      </Routes>
+    </Router>
   );
 }
 
